249422a3c2a86fbe23ff13ad0a43f6217e555c7d
kansuki20 <kansuki20@gmail.com>
2025-04-18 18:59:52 +0900

    init: project setting

---
+++ src/main/kotlin/dev/kansuki20/resultmapbuilder/GenerateResultMapAction.kt
@@ -0,0 +1,47 @@
+package dev.kansuki20.resultmapbuilder
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.ide.CopyPasteManager
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiJavaFile
+import java.awt.datatransfer.StringSelection
+
+class GenerateResultMapAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val psiFile: PsiFile? = e.getData(CommonDataKeys.PSI_FILE)
+        if (psiFile !is PsiJavaFile) return
+
+        if (psiFile.classes.isEmpty()) return
+
+        val clazz: PsiClass = psiFile.classes.first()
+
+        val sb = StringBuilder()
+        sb.append("<resultMap id=\"")
+            .append(clazz.name!!.replaceFirstChar { it.lowercase() })
+            .append("Map\" type=\"")
+            .append(clazz.qualifiedName)
+            .append("\">\n")
+
+        for (field in clazz.allFields) {
+            val name = field.name
+            sb
+                .append("  <result property=\"").append(name)
+                .append("\" column=\"")
+                .append(name.toSnakeCase())
+                .append("\"/>\n")
+        }
+
+        sb.append("</resultMap>")
+        CopyPasteManager.getInstance().setContents(StringSelection(sb.toString()))
+    }
+
+
+    private fun String.toSnakeCase(): String {
+        return this
+            .replace(Regex("([a-z])([A-Z]+)"), "$1_$2")
+            .lowercase()
+    }
+}
